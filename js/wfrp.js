9b992383f5d63aed4fe3938b9886ea4323c30996
André <137167063+AndreK-B06@users.noreply.github.com>
2024-02-27 09:58:33 +0100

    working on random rolls

--- js/wfrp.js
+++ js/wfrp.js
@@ -1,9 +1,38 @@
 document.addEventListener("DOMContentLoaded", (e) => {
+  //
+  // * Get Element from Html *//
+  //
+  let diceTen = document.querySelector("#dice10");
+  let diceThousand = document.querySelector("#dice100");
+  let result = document.querySelector("#dice-result");
+  const attack = document.querySelector("#attack-btn");
+  const defence = document.querySelector("#defence-btn");
+  //
+  // * Define dices *//
+  //
+  let sidesOnDice = 10;
+  //
+  // * Create Element *//
+  //
+  let newParagraph = document.createElement("p");
+
+  //
   // * Define pages *//
+  //
   const homePage = document.querySelector("#home-btn");
   const warhammerFantasy = document.querySelector("#wfrp");
-
+  //
+  // * Dices function *//
+  //
+  attack.addEventListener("click", function () {
+    let rolledDiceTen = Math.floor(Math.random() * sidesOnDice) + 1;
+    result.appendChild(newParagraph);
+    newParagraph.textContent = "Result: ${rollecDiceTen";
+    console.log(diceten);
+  });
+  //
   // * Page switchers *//
+  //
   homePage.addEventListener("click", function () {
     location.href = "./home.html";
   });
